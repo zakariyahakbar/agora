520224b0a9a52369dec57302cfbf5d43dd377fee
zakariyah akbar <zakariyah.akbar16@gmail.com>
2026-05-26 15:36:07 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -38,6 +38,9 @@ export default function RootLayout({ children }) {
       }}
     >
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
